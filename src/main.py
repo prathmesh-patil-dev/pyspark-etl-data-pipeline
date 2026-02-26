9b2388462dabbd72b5bd8bb412261ffc25199f76
prathmesh-patil-dev <prathmeshp183@gmail.com>
2026-02-26 23:10:33 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,14 +1,19 @@
+from pyspark.sql import SparkSession
 from extract import extract_data
 from transform import transform_data
 from load import load_data
+from config.config_dev import INPUT_PATH, OUTPUT_PATH
 
 def main():
-    input_path = "data/raw/sample.csv"
-    output_path = "data/processed/"
+    spark = SparkSession.builder \
+        .appName("Enterprise ETL Pipeline") \
+        .getOrCreate()
 
-    df = extract_data(input_path)
+    df = extract_data(spark, INPUT_PATH)
     transformed_df = transform_data(df)
-    load_data(transformed_df, output_path)
+    load_data(transformed_df, OUTPUT_PATH)
+
+    spark.stop()
 
 if __name__ == "__main__":
     main()
